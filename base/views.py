7aae2b4753c665de208eac8f459f2ef78139e058
GauravLamichhane <gaurav.lamichhane02@gmail.com>
2025-05-12 20:48:35 +0545

    Added new functionality

--- base/views.py
+++ base/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 
-rooms = [
-  {'id': 1, 'name':'Learnpython'},
-  {'id': 2, 'name':'FrontEnd'},
-]
-
+# rooms = [
+#   {'id': 1, 'name':'Learn python'},
+#   {'id': 2, 'name':'Front End'},
+#   {'id':3,'name':'React Native'}
+# ]
+from .models import Room
 def home(request):
-  return render(request,'home.html',{'rooms':rooms})
+  rooms = Room.objects.all()
+  context = {'rooms':rooms}
+  return render(request,'base/home.html',context)
 
-def room(request):
-  return render(request,'room.html')
+def room(request,pk):
+  room = Room.objects.get(id = pk)
+  context = {'room':room}
+  return render(request,'base/room.html',context)
